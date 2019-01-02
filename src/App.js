f028834fd0ae89460aad62eae83f6d6fd1483e19
Nate Baer <njbbaer@gmail.com>
2019-01-01 18:58:55 -0800

    Add text input with character counter

--- src/App.js
+++ src/App.js
@@ -3,22 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    textLength: 0
+  }
+
+  textLengthHandler = (event) => {
+    this.setState({textLength: event.target.value.length});
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <input type="text" onChange={this.textLengthHandler}></input>
+          <p>{this.state.textLength} chars</p>
         </header>
       </div>
     );
